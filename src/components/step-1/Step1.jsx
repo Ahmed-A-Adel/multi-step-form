427356fb62dd9f42529c0c57061508a2a4b7c0c1
Ahmed Adel <AhmedAdel.PersonalEmail@gmail.com>
2023-01-03 10:47:39 +0200

    added some info about steps in step-1 with some styling

--- src/components/step-1/Step1.jsx
+++ src/components/step-1/Step1.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 import "./Step1.scss";
 function Step1() {
-  return <div className="step"> step 1</div>;
+  const stepInfo = {
+    1: {
+      heading: " Personal info",
+      paragraph: " Please provide your name, email address, and phone number.",
+    },
+    2: {
+      heading: "  Select your plan",
+      paragraph: " You have the option of monthly or yearly billing. ",
+    },
+    3: {
+      heading: "Pick add-ons",
+      paragraph: "Add-ons help enhance your gaming experience.",
+    },
+    4: {
+      heading: "Finishing up",
+      paragraph: " Double-check everything looks OK before confirming.",
+    },
+    5: {
+      heading: "Thank you!",
+      paragraph:
+        " Thanks for confirming your subscription! We hope you have funusing our platform. If you ever need support, please feel free to email us at support@loremgaming.com.",
+    },
+  };
+
+  return (
+    <div className="step">
+      <h2 className="step_heading">{stepInfo[1].heading}</h2>
+      <p className="step_text">{stepInfo[1].paragraph}</p>
+    </div>
+  );
 }
 
 export default Step1;
