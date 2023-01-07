42e5c3c0b06c086a1154cefb71e55b849d7284c3
Ahmed Adel <AhmedAdel.PersonalEmail@gmail.com>
2023-01-07 06:44:18 +0200

    added footer buttons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Footer.scss";
 function Footer() {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <Link className="btn btn-back"> go back</Link>
+      <Link className="btn btn-next">next step</Link>
+    </div>
+  );
 }
 
 export default Footer;
