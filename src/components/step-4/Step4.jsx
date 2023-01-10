e347fddccf4fffdef94a8d715e6aee30b87a0bcf
Ahmed Adel <AhmedAdel.PersonalEmail@gmail.com>
2023-01-11 01:59:53 +0200

    aded Summary component to step-4

--- src/components/step-4/Step4.jsx
+++ src/components/step-4/Step4.jsx
@@ -1,7 +1,23 @@
 import React from "react";
-
+import Heading from "../heading/Heading";
+import Summary from "../summary/Summary";
+import "./Step4.scss";
 function Step4() {
-  return <div>Step4</div>;
+  return (
+    <div className="step step-4">
+      <Heading
+        heading={"Finishing up"}
+        subHeading={" Double-check everything looks OK before confirming."}
+      />
+      <Summary
+        plane={{ name: "arcade", price: 9, time: "monthly" }}
+        addOnes={[
+          { name: "online service", price: 1 },
+          { name: "larger storage", price: 2 },
+        ]}
+      />
+    </div>
+  );
 }
 
 export default Step4;
---
+++ src/components/summary/Summary.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Summary.scss";
+function Summary({ plane, addOnes }) {
+  // ___________________________________________________________________
+  const total = plane.price + addOnes[0].price + addOnes[1].price;
+  // ----------------------------------------------------------------
+  const summaryPlaneHead = ` ${plane.name} (${plane.time})`;
+  // ----------------------------------------------------------------
+  const summaryPlanePrice = `$${plane.price}/${plane.time.slice(0, 2)}`;
+  // ----------------------------------------------------------------
+  const addOnesElements = addOnes.map((add) => (
+    <div className="summary_add-ones">
+      <p>{add.name}</p>
+      <span>{`+$${add.price}/${plane.time.slice(0, 2)}`}</span>
+    </div>
+  ));
+  // ----------------------------------------------------------------
+  const summaryTotalPrice = `+$${total}/${plane.time.slice(0, 2)}`;
+  // ----------------------------------------------------------------
+  const summaryTotalHead = `Total (per ${plane.time.slice(0, -2)})`;
+  // ___________________________________________________________________
+
+  return (
+    <div className="summary">
+      <div className="summary_container">
+        <div className="summary_plane">
+          <h5> {summaryPlaneHead}</h5>
+          <span className="summary_plane_price">{summaryPlanePrice}</span>
+        </div>
+        <Link>change</Link>
+        <div className="summary_line"></div>
+        {addOnesElements}
+      </div>
+      <div className="summary_total">
+        <p>{summaryTotalHead}</p>
+        <span>{summaryTotalPrice}</span>
+      </div>
+    </div>
+  );
+}
+
+export default Summary;
