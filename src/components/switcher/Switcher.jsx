23e5e491fd39dd1c28689799a93d99e40063a1aa
Ahmed Adel <AhmedAdel.PersonalEmail@gmail.com>
2023-01-13 09:41:00 +0200

    added switcher toggling functionality

--- src/components/switcher/Switcher.jsx
+++ src/components/switcher/Switcher.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Switcher.scss";
 function Switcher() {
+  const [state, setState] = useState(false);
+  const handleClick = () => setState(!state);
   return (
-    <div className="switcher">
+    <div className="switcher" onClick={handleClick}>
       <span className="switcher_option">monthly</span>
       <div className="switcher_container">
-        <div className="switcher_container_ball"></div>
+        <div
+          className={`switcher_container_ball ${state && "switcher_right"}`}
+        ></div>
       </div>
       <span className="switcher_option">yearly</span>
     </div>
