8b9eaa3b5b922ca392daad84433839bff6a47514
Ahmed Adel <AhmedAdel.PersonalEmail@gmail.com>
2023-01-14 08:04:31 +0200

    replace the routing logic from footer to App.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import "./Footer.scss";
 function Footer({ step, handleLastStep, handleNextStep }) {
-  const nexStep = step === "/" ? "/step-2" : `/step-${+step.slice(-1) + 1}`;
-  const prevStep = step === "/step-2" ? "/" : `/step-${+step.slice(-1) - 1}`;
-
   return (
     <div className="footer">
       {step !== "/" && (
-        <Link to={prevStep} className="btn btn-back" onClick={handleLastStep}>
+        <Link className="btn btn-back" onClick={handleLastStep}>
           go back
         </Link>
       )}
       {step !== "/step-5" && (
-        <Link to={nexStep} className="btn btn-next" onClick={handleNextStep}>
+        <Link className="btn btn-next" onClick={handleNextStep}>
           next step
         </Link>
       )}
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,23 +1,34 @@
 import { useState } from "react";
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import Sidebar from "../sidebar/Sidebar";
 import Footer from "../Footer/Footer";
 import "./App.scss";
 function App() {
   // __________ Varibels __________________________________
   const [state, setState] = useState({ step: "/" });
+  const navigate = useNavigate();
   // __________ Varibels __________________________________
   // _____________________ Functions _______________________________
-  const handleNextStep = () => {
+  const handleNextStep = (e) => {
+    e.preventDefault();
+
     const currentStep =
       state.step === "/" ? "/step-2" : `/step-${+state.step.slice(-1) + 1}`;
+
     setState({ ...state, step: currentStep });
+
+    navigate(currentStep);
   };
   // ________________________________________________________________
-  const handleLastStep = () => {
+  const handleLastStep = (e) => {
+    e.preventDefault();
+
     const currentStep =
       state.step === "/step-2" ? "/" : `/step-${+state.step.slice(-1) - 1}`;
+
     setState({ ...state, step: currentStep });
+
+    navigate(currentStep);
   };
   // _____________________ Functions _______________________________
 
