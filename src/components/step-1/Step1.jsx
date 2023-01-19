cb41ad418a39f89b70d861e610c2d5e70996ab5c
Ahmed Adel <AhmedAdel.PersonalEmail@gmail.com>
2023-01-19 09:39:50 +0200

    deleted step-1 headingInfo object with normal text

--- src/components/step-1/Step1.jsx
+++ src/components/step-1/Step1.jsx
@@ -4,29 +4,6 @@ import Heading from "../heading/Heading";
 import useInput from "../../hooks/useInput";
 import "./Step1.scss";
 function Step1() {
-  const stepInfo = {
-    1: {
-      heading: " Personal info",
-      paragraph: " Please provide your name, email address, and phone number.",
-    },
-    2: {
-      heading: "  Select your plan",
-      paragraph: " You have the option of monthly or yearly billing. ",
-    },
-    3: {
-      heading: "Pick add-ons",
-      paragraph: "Add-ons help enhance your gaming experience.",
-    },
-    4: {
-      heading: "Finishing up",
-      paragraph: " Double-check everything looks OK before confirming.",
-    },
-    5: {
-      heading: "Thank you!",
-      paragraph:
-        " Thanks for confirming your subscription! We hope you have funusing our platform. If you ever need support, please feel free to email us at support@loremgaming.com.",
-    },
-  };
   const [inputValue, handleInput] = useInput("");
   const [emailValue, handleEmail] = useInput("");
   const [phoneValue, handlePhone] = useInput("");
@@ -34,8 +11,10 @@ function Step1() {
   return (
     <div className="step step-1">
       <Heading
-        heading={stepInfo[1].heading}
-        subHeading={stepInfo[1].paragraph}
+        heading={" Personal info"}
+        subHeading={
+          " Please provide your name, email address, and phone number."
+        }
       />
       <form>
         <label htmlFor="name">Name</label>
