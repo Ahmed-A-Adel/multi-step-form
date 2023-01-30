ddac4d3d823b4a5697e7395a0a7e5f68fb89613f
Ahmed Adel <AhmedAdel.PersonalEmail@gmail.com>
2023-01-30 10:46:43 +0200

    replaced step-1 multiple useInput with single useState object

--- src/components/step-1/Step1.jsx
+++ src/components/step-1/Step1.jsx
@@ -6,11 +6,18 @@ import { stepsContext } from "../StepsProvider";
 import "./Step1.scss";
 import Button from "../button/Button";
 function Step1() {
-  const [inputValue, handleInput] = useInput("");
-  const [emailValue, handleEmail] = useInput("");
-  const [phoneValue, handlePhone] = useInput("");
+  const [formValues, setFormValues] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+  const formElementsData = [{ name: "user name" }];
   const { dispatch, state } = useContext(stepsContext);
+  // __________ Functions ________________________________
+  const handleChange = (e) =>
+    setFormValues({ ...formValues, [e.target.name]: e.target.value });
 
+  // __________ Functions ________________________________
   return (
     <div className="step step-1">
       <Heading
@@ -23,27 +30,30 @@ function Step1() {
         <label htmlFor="name">Name</label>
         <input
           id="name"
+          name="name"
           type={"text"}
-          value={inputValue}
-          onChange={handleInput}
+          value={formValues.name}
+          onChange={handleChange}
           placeholder="e.g. Ahmed A Adel"
         />
 
         <label htmlFor="email">Email Address</label>
         <input
           id="email"
+          name="email"
           type={"email"}
-          value={emailValue}
-          onChange={handleEmail}
+          value={formValues.email}
+          onChange={handleChange}
           placeholder="e.g. ahmedadel.personalemail@gmail.com"
         />
 
         <label htmlFor="phone">Phone Number</label>
         <input
           id="phone"
+          name="phone"
           type={"text"}
-          value={phoneValue}
-          onChange={handlePhone}
+          value={formValues.phone}
+          onChange={handleChange}
           placeholder="e.g. +20-1 234 567 89"
         />
       </form>
